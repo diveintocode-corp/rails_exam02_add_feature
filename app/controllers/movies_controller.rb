d4a893c8802c3ab8d2b25a6c8cc044baaa26f497
yuji91 <yuji91.1227@gmail.com>
2019-06-01 15:56:57 +0900

    style: remove comments

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,23 +1,18 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: %i[show edit update destroy]
 
-  # GET /movies
   def index
     @movies = Movie.all
   end
 
-  # GET /movies/1
   def show; end
 
-  # GET /movies/new
   def new
     @movie = Movie.new
   end
 
-  # GET /movies/1/edit
   def edit; end
 
-  # POST /movies
   def create
     @movie = Movie.new(movie_params)
 
@@ -28,7 +23,6 @@ class MoviesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /movies/1
   def update
     if @movie.update(movie_params)
       redirect_to @movie, notice: 'Movie was successfully updated.'
@@ -37,7 +31,6 @@ class MoviesController < ApplicationController
     end
   end
 
-  # DELETE /movies/1
   def destroy
     @movie.destroy
     redirect_to movies_url, notice: 'Movie was successfully destroyed.'
@@ -45,12 +38,10 @@ class MoviesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_movie
     @movie = Movie.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def movie_params
     params.require(:movie).permit(:title, :summary)
   end
