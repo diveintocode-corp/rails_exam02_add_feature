b2ed0c18ac41f955e5a19fe8ab9c7af3bc0f060f
yuji91 <yuji91.1227@gmail.com>
2019-06-08 21:53:36 +0900

    Modify routing and view files by Wiki

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
+  root :to => 'users#index'
+  resources :user_sessions
   resources :users
   resources :movies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
