933d146f0a837d178e7442e75c6095ba9ac396ce
yuji91 <yuji91.1227@gmail.com>
2019-06-08 22:00:11 +0900

    Add before_action to controllers by Wiki

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:index, :new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -23,8 +24,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
+    # if @user.save
+    #   redirect_to @user, notice: 'User was successfully created.'
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+        redirect_to(:users, notice: 'User was successfully created')
     else
       render :new
     end
