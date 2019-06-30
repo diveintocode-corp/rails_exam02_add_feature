3ddc0312308a0289d0ec0de020abbe7b8781060a
yuji91 <yuji91.1227@gmail.com>
2019-06-30 19:50:53 +0900

    Fix: 許可したModelClass以外で、Validatorを使った場合に例外を発生させる

--- app/validators/password_validator.rb
+++ app/validators/password_validator.rb
@@ -1,7 +1,10 @@
 class PasswordValidator < ActiveModel::EachValidator
+  PERMITTED_CLASS = ['User'].freeze
   PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.freeze
 
   def validate_each(record, attribute, value)
+    raise 'UnpermittedClassError' unless PERMITTED_CLASS.include?(record.class.name)
+
     return unless record.new_record? || record.changes[:crypted_password]
 
     record.errors.add(attribute, "can't be blank") if value.blank?
